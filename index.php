9d58e174686b544e067e8707b1cde7853ffa8b52
gabriella-silva <gabriellaferreira534@gmail.com>
2024-12-08 14:43:51 -0300

    <3

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $result = $stmt->get_result();
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Panorama Urbano</title>
+    <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
    
 <!--Fontes-->
--- readme.txt
+++
@@ -1,6 +0,0 @@
-Notas importantesssss
-
-O design do login seja do adm ou do usúario são feito no "login.css" pq ele vai ser o padrão, se quiserem mudar algo em um form especifico 
-PRINCIPALMENTE VC MIGUEL, faz inline e não na raiz, obrigada. Todo o resto como são pgs muito especificas tem css inline 
-
-coloquei num readme pq talvez eu falte a semana toda e ai vcs vão se fuder
